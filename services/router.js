780fcf7454bf43f0a05aa01b1ecc1a21e104d849
Maria <marijakasmina99@gmail.com>
2021-02-21 18:41:56 +0300

    add flight api

--- services/router.js
+++ services/router.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const router = new express.Router();
 const passenger = require('../controllers/passenger.js');
-const flight = require('../controllers/flight.js');
+const flight = require('../controllers/flight');
 
 // знак ? после id указывает на его необязательность
 router.route('/passenger/:id?')
@@ -14,7 +14,7 @@ router.route('/passenger/:id?')
 router.route('/').get();
 
 router.route('/flight/msk/spb').get(flight.getInfoAboutFlight);
-router.route('/flight/airline/:id').get();
+router.route('/flight/airline/:id').get(flight.getInfoAboutAirlineById);
 
 
 
